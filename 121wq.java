d2c700ba7682c89e0dcc542ea25e49781ef6b073
jamieInston <artembojko038@gmail.com>
2026-01-08 14:21:08 +0200

    Refactor code to use new input stream variables

--- 121wq.java
+++ 121wq.java
@@ -1,26 +1,27 @@
 var ctx = android.app.ActivityThread.currentApplication().getApplicationContext();
 String deviceId = android.provider.Settings.Secure.getString(ctx.getContentResolver(), android.provider.Settings.Secure.ANDROID_ID);
 
-var url = new URL("https://raw.githubusercontent.com/artembojko038-afk/home_work/main/chemistry.txt");
-var conn = url.openConnection();
-var iS = conn.getInputStream(); 
-var br = new BufferedReader(new InputStreamReader(iS));
+var url2 = new URL("https://raw.githubusercontent.com/artembojko038-afk/home_work/main/chemistry.txt");
+var conn2 = url2.openConnection();
+var iS2 = conn2.getInputStream(); 
+var br2 = new BufferedReader(new InputStreamReader(iS2));
 
 String line;
 var keys = new ArrayList<String>();
-while ((line = br.readLine()) != null) {
+while ((line = br2.readLine()) != null) {
     line = line.trim();
     if (!line.isEmpty()) {
         keys.add(line);   
     }
 }
 
-br.close();
-iS.close();
+br2.close();
+iS2.close();
 
-if (keys.contains("357d78c945779392")) {
+if (keys.contains("deviceId")) {
     pfc.log("Valid!");
 } else { 
     pfc.log("Not Valid!");
 }
 
+pfc.log("True");
